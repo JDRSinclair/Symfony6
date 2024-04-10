95eda5f0b1224f85a584c376102dc7f7eb3f8a93
Juan David RODRIGUEZ SINCLAIR <juandavidrodriguezsinclair@gmail.com>
2024-04-10 16:55:19 +0200

    index.php

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$url = $request->getPathInfo();
+$response = new Response();
+
+switch($url) {
+    case '/':
+        $response->setContent('Accueil');
+        break;
+    case '/admin':
+        $response->setContent('Accès Espace Admin');
+        break;
+    default:
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+}
+
+$response->send();
